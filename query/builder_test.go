3741c3a90f1bd9ef881c22c7c9f7b38e39691302
Ivan Startsev <i.startsev@postgrespro.ru>
2023-04-22 11:08:14 +0300

    QB-1: refactoring of makefile: bench target

--- query/builder_test.go
+++ query/builder_test.go
@@ -240,14 +240,19 @@ func BenchmarkPGBuilderPlain(b *testing.B) {
 		args          []any
 	)
 	qb := pg.NewQueryBuilder()
+
+	getPrepBuild := func() query.Builder {
+		return qb.Select("one", "two", "three").
+			From("table 1").
+			Where("id").Equal(1).
+			Where("name").In("n1", "n2", "n3").
+			Where("count").Between(1, 100).
+			Limit(100).Offset(100)
+	}
+	preparedQuery = getPrepBuild()
 	b.Run("prepare query", func(b *testing.B) {
 		for i := 0; i < b.N; i++ {
-			preparedQuery = qb.Select("one", "two", "three").
-				From("table 1").
-				Where("id").Equal(1).
-				Where("name").In("n1", "n2", "n3").
-				Where("count").Between(1, 100).
-				Limit(100).Offset(100)
+			preparedQuery = getPrepBuild()
 		}
 	})
 
