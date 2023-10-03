826bc3f80afd225f3aa703ec318d2501f049f1c5
Ivan Startsev <i.startsev67@gmail.com>
2023-10-03 23:52:42 +0300

    #9 attemp of optimization heap allocations

--- go.mod
+++ go.mod
@@ -7,5 +7,4 @@ require github.com/stretchr/testify v1.2.2
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/tools v0.8.0 // indirect
 )
--- internal/query/where.go
+++ internal/query/where.go
@@ -75,6 +75,19 @@ func (w *Where) String(idBuilder *identity.Builder) string {
 	panic("unknown where condition")
 }
 
+const numsStr = "$1, $2, $3, $4, $5, $6, $7, $8, $9,$10,$11,$12,$13,$14,$15,$16,$17,$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30,$31,$32,$33,$34,$35,$36,$37,$38,$39"
+
+func getNums(s, cnt int) string {
+	if s == 0 {
+		return ""
+	}
+	s--
+	start := s * 4
+	pos := start + (cnt-1)*4 + 2
+
+	return numsStr[start:pos]
+}
+
 func (w *Where) PrepStmtString(num int, wr io.Writer, idBuilder *identity.Builder) ([]any, error) {
 	switch w.cond {
 	case eq, ne, le, lq, gt, gq, like, notLike:
@@ -83,12 +96,7 @@ func (w *Where) PrepStmtString(num int, wr io.Writer, idBuilder *identity.Builde
 		}
 		return w.value, nil
 	case in, notIn:
-		nums := make([]string, len(w.value))
-		for i := range w.value {
-			nums[i] = fmt.Sprintf("$%d", num)
-			num++
-		}
-		if _, err := fmt.Fprintf(wr, "%s %s (%s)", idBuilder.Ident(w.field), w.cond.String(), strings.Join(nums, ", ")); err != nil {
+		if _, err := fmt.Fprintf(wr, "%s %s (%s)", idBuilder.Ident(w.field), w.cond.String(), getNums(num, len(w.value))); err != nil {
 			return nil, fmt.Errorf("write into writer: %w", err)
 		}
 
@@ -100,13 +108,7 @@ func (w *Where) PrepStmtString(num int, wr io.Writer, idBuilder *identity.Builde
 
 		return nil, nil
 	case between, notBetween:
-		nums := make([]string, len(w.value))
-		for i := range w.value {
-			nums[i] = fmt.Sprintf("$%d", num)
-			num++
-		}
-
-		if _, err := fmt.Fprintf(wr, "%s %s %s AND %s", idBuilder.Ident(w.field), w.cond.String(), nums[0], nums[1]); err != nil {
+		if _, err := fmt.Fprintf(wr, "%s %s $%d AND $%d", idBuilder.Ident(w.field), w.cond.String(), num, num+1); err != nil {
 			return nil, fmt.Errorf("write into writer: %w", err)
 		}
 
@@ -118,42 +120,42 @@ func (w *Where) PrepStmtString(num int, wr io.Writer, idBuilder *identity.Builde
 
 func (w *WherePart[T]) Equal(v any) T {
 	w.Where.cond = eq
-	w.Where.value = []any{v}
+	w.Where.value = append(w.Where.value, v)
 
 	return w.b
 }
 
 func (w *WherePart[T]) NotEqual(v any) T {
 	w.Where.cond = ne
-	w.Where.value = []any{v}
+	w.Where.value = append(w.Where.value, v)
 
 	return w.b
 }
 
 func (w *WherePart[T]) Less(v any) T {
 	w.Where.cond = le
-	w.Where.value = []any{v}
+	w.Where.value = append(w.Where.value, v)
 
 	return w.b
 }
 
 func (w *WherePart[T]) LessEqual(v any) T {
 	w.Where.cond = lq
-	w.Where.value = []any{v}
+	w.Where.value = append(w.Where.value, v)
 
 	return w.b
 }
 
 func (w *WherePart[T]) Greater(v any) T {
 	w.Where.cond = gt
-	w.Where.value = []any{v}
+	w.Where.value = append(w.Where.value, v)
 
 	return w.b
 }
 
 func (w *WherePart[T]) GreaterEqual(v any) T {
 	w.Where.cond = gq
-	w.Where.value = []any{v}
+	w.Where.value = append(w.Where.value, v)
 
 	return w.b
 }
@@ -185,20 +187,14 @@ func (w *WherePart[T]) IsNotNull() T {
 }
 
 func (w *WherePart[T]) Between(start, end any) T {
-	w.Where.value = []any{
-		start,
-		end,
-	}
+	w.Where.value = append(w.Where.value, start, end)
 	w.Where.cond = between
 
 	return w.b
 }
 
 func (w *WherePart[T]) NotBetween(start, end any) T {
-	w.Where.value = []any{
-		start,
-		end,
-	}
+	w.Where.value = append(w.Where.value, start, end)
 	w.Where.cond = notBetween
 
 	return w.b
@@ -206,14 +202,14 @@ func (w *WherePart[T]) NotBetween(start, end any) T {
 
 func (w *WherePart[T]) Like(pattern string) T {
 	w.Where.cond = like
-	w.Where.value = []any{pattern}
+	w.Where.value = append(w.Where.value, pattern)
 
 	return w.b
 }
 
 func (w *WherePart[T]) NotLike(pattern string) T {
 	w.Where.cond = notLike
-	w.Where.value = []any{pattern}
+	w.Where.value = append(w.Where.value, pattern)
 
 	return w.b
 }
